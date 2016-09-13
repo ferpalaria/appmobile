0e8742a4b0e1a7561bc7c3bd5ad8cbe274d20f8a
Fernanda <frpalaria@gmail.com>
2016-09-13 01:25:02 -0300

    Adicionando Layout das telas - Login e Modulos

--- AprendendoIoT/app/src/main/java/com/br/fpalaria/aprendendoiot/Modulos.java
+++ AprendendoIoT/app/src/main/java/com/br/fpalaria/aprendendoiot/Modulos.java
@@ -1,5 +1,7 @@
 package com.br.fpalaria.aprendendoiot;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class Modulos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modulos);
+
+
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.mipmap.ic_flight_black_24dp);
+
     }
 }
