8d5a3bb3138ce1fec40c695fb7b0683bdf4e01e5
Fernanda <frpalaria@gmail.com>
2016-09-12 22:54:10 -0300

    criando projeto Android Studio - aprendendoIoT

---
+++ AprendendoIoT/app/src/main/java/com/br/fpalaria/aprendendoiot/Topicos.java
@@ -0,0 +1,13 @@
+package com.br.fpalaria.aprendendoiot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Topicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topicos);
+    }
+}
